fc73d9493fc463b1b26b4e264d8528aed22a0970
David Kayode <davidkayode009@live.com>
2022-09-16 09:51:48 -0400

    move app images to new folder

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ window.title('Password Manager')
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
-logo = PhotoImage(file='logo.png')
+logo = PhotoImage(file='appImages/logo.png')
 canvas.create_image(100, 100, image=logo)
 canvas.grid(column=1, row=0)
 
